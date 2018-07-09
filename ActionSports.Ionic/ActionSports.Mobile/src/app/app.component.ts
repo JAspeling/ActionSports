94529b637734f6b8db9df28b5f8b93a7b4f3d3da
Johan Aspeling <aspeling.johan@gmail.com>
2018-07-09 12:48:08 +0200

    Updated Angular + Ionic, pointed api to Amazon host

--- ActionSports.Ionic/ActionSports.Mobile/src/app/app.component.ts
+++ ActionSports.Ionic/ActionSports.Mobile/src/app/app.component.ts
@@ -18,7 +18,8 @@ export class MyApp {
     // storage.set('api', 'http://action.jaspeling.co.za/ActionSports.API');
     // AppState.basePage = 'http://action.jaspeling.co.za/ActionSports.API/api'
     // AppState.basePage = 'http://localhost:5000/api'
-    AppState.basePage = 'http://jaspeling.grifdev.co.za/api/';
+    // AppState.basePage = 'http://jaspeling.grifdev.co.za/api/';
+    AppState.basePage = 'http://ec2-18-219-81-189.us-east-2.compute.amazonaws.com/ActionSports.API/api';
 
     storage.forEach((value, key, index) => {
       console.log(`${key} - ${value}`);
